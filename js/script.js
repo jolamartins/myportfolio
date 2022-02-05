e71eac7183ddd4660fbb96817bceded32cb5192c
Jola Martins <tianashade@gmail.com>
2022-02-05 08:40:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+document.getElementById('box').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+})
+
+document.getElementById('box2').addEventListener('click', function(){
+    document.querySelector('.bg-modal2').style.display = 'flex';
+})
+
+document.querySelector('.close-modal2').addEventListener('click', function(){
+    document.querySelector('.bg-modal2').style.display = 'none';
+})
+
+document.getElementById('box3').addEventListener('click', function(){
+    document.querySelector('.bg-modal3').style.display = 'flex';
+})
+
+document.querySelector('.close-modal3').addEventListener('click', function(){
+    document.querySelector('.bg-modal3').style.display = 'none';
+})
+
+document.getElementById('box4').addEventListener('click', function(){
+    document.querySelector('.bg-modal4').style.display = 'flex';
+})
+
+document.querySelector('.close-modal4').addEventListener('click', function(){
+    document.querySelector('.bg-modal4').style.display = 'none';
+})
+
+document.getElementById('box5').addEventListener('click', function(){
+    document.querySelector('.bg-modal5').style.display = 'flex';
+})
+
+document.querySelector('.close-modal5').addEventListener('click', function(){
+    document.querySelector('.bg-modal5').style.display = 'none';
+})
+
+document.getElementById('box6').addEventListener('click', function(){
+    document.querySelector('.bg-modal6').style.display = 'flex';
+})
+
+document.querySelector('.close-modal6').addEventListener('click', function(){
+    document.querySelector('.bg-modal6').style.display = 'none';
+})
+
+document.getElementById('box7').addEventListener('click', function(){
+    document.querySelector('.bg-modal7').style.display = 'flex';
+})
+
+document.querySelector('.close-modal7').addEventListener('click', function(){
+    document.querySelector('.bg-modal7').style.display = 'none';
+})
+
+document.getElementById('box8').addEventListener('click', function(){
+    document.querySelector('.bg-modal8').style.display = 'flex';
+})
+
+document.querySelector('.close-modal8').addEventListener('click', function(){
+    document.querySelector('.bg-modal8').style.display = 'none';
+})
+
+document.getElementById('box9').addEventListener('click', function(){
+    document.querySelector('.bg-modal9').style.display = 'flex';
+})
+
+document.querySelector('.close-modal9').addEventListener('click', function(){
+    document.querySelector('.bg-modal9').style.display = 'none';
+})
+
+document.getElementById('box10').addEventListener('click', function(){
+    document.querySelector('.bg-modal10').style.display = 'flex';
+})
+
+document.querySelector('.close-modal10').addEventListener('click', function(){
+    document.querySelector('.bg-modal10').style.display = 'none';
+})
+
+document.getElementById('box11').addEventListener('click', function(){
+    document.querySelector('.bg-modal11').style.display = 'flex';
+})
+
+document.querySelector('.close-modal11').addEventListener('click', function(){
+    document.querySelector('.bg-modal11').style.display = 'none';
+})
+
+document.getElementById('box12').addEventListener('click', function(){
+    document.querySelector('.bg-modal12').style.display = 'flex';
+})
+
+document.querySelector('.close-modal12').addEventListener('click', function(){
+    document.querySelector('.bg-modal12').style.display = 'none';
+})
+
+
